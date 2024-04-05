d8a393e121834736f9fd89e1b2c02470e142f8af
qwexx83 <stanislavpesko64@gmail.com>
2024-04-05 12:22:43 +0300

    Lab12

---
+++ Lab12/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Tester test = new Tester();
+        test.Method();
+        test.Method("Метод 2");
+        test.Method("5" , "sdfsfgf");
+        test.MethodStat();
+    }
+}
